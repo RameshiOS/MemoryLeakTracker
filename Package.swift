fc2cd971f813a75f63b02fc8c8e19fea194cdee7
RameshIOS <ramesh.guddala@gmail.com>
2025-03-16 18:17:33 +0530

    updated min version

--- Package.swift
+++ Package.swift
@@ -5,6 +5,8 @@ import PackageDescription
 
 let package = Package(
     name: "MemoryLeakTracker",
+    platforms: [.iOS(.v13)], // Supports iOS 13+
+
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
